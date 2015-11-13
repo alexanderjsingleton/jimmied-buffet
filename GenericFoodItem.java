c328feafc13910a6e569bab30eff8966d8565537
Alex Singleton <alexanderjsingleton@gmail.com>
2015-11-12 19:19:20 -0600

    added 2 instance fields in your GenericFoodItem class fields

---
+++ GenericFoodItem.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class GenericFoodItem here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GenericFoodItem extends Actor
+{
+    protected boolean inLeftHalf = true;
+    protected int        caloriesForItem    = 10;
+    /**
+     * Act - do whatever the GenericFoodItem wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
