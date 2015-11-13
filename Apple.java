e58a61577e761f0d0ab824a209dbde512db4ab75
Alex Singleton <alexanderjsingleton@gmail.com>
2015-11-12 19:31:36 -0600

    Inheritance for our specific food items

---
+++ Apple.java
@@ -0,0 +1,25 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Apple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Apple extends GenericFoodItem
+{
+    public Apple()
+    {
+        super();
+        caloriesForItem = 95;
+    }
+
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        super.act();
+    }    
+}
