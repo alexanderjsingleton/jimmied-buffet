6b92e6d806e56b00b2de2aabed368f98028d4c01
Alex Singleton <alexanderjsingleton@gmail.com>
2015-11-12 19:34:27 -0600

    bread and butter methods

---
+++ Bread.java
@@ -0,0 +1,24 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Bread here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bread extends GenericFoodItem
+{
+    public Bread()
+    {
+        super();
+        caloriesForItem = 210;
+    }
+    /**
+     * Act - do whatever the Bread wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       super.act();
+    }    
+}
